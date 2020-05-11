6a69e9171fbd917c4e51bc1a78f85e5ab055d8f9
bigdot123456 <bigdot@126.com>
2020-05-11 17:14:41 +0800

    ok for normal info

--- MQTTClientWithDB.py
+++ MQTTClientWithDB.py
@@ -18,23 +18,28 @@ SQLCODE = "mysql+pymysql://tiger:test123456!@@127.0.0.1/test?charset=utf8"
 
 MQTTHOST = '111.229.168.108'
 MQTTPORT = 1883
-clientid = 'test_mqtt_python_' + str(uuid.uuid4())
 username = 'userA'
 password = 'userfast'
-topic = 'mtopic'
 
-mqttClient= mqtt.Client(f"{clientid}|securemode=3,signmethod=hmacsha1|")
-mqttClient.username_pw_set(username, password)
 
 class MQTTClientWithDB():
+    ptopic=""
+    stopic=""
+    mqttClient = mqtt.Client(f"goog|securemode=3,signmethod=hmacsha1|")
+    mqttClient.username_pw_set(username, password)
+
+    def __init__(self, clientid,ptopic,stopic):
+        self.clientid = clientid
+        self.ptopic=ptopic
+        self.stopic=stopic
 
     def on_mqtt_connect(self):
-        mqttClient.connect(MQTTHOST, MQTTPORT, 60)
-        mqttClient.loop_start()
+        self.mqttClient.connect(MQTTHOST, MQTTPORT, 60)
+        self.mqttClient.loop_start()
 
     # publish 消息
     def on_publish(self,topic, payload, qos):
-        mqttClient.publish(topic, payload, qos)
+        self.mqttClient.publish(topic, payload, qos)
 
     # 消息处理函数
     def on_message_come(self,client, userdata, msg):
@@ -43,31 +48,35 @@ class MQTTClientWithDB():
     # subscribe 消息
     def on_subscribe(self):
         # 订阅监听自定义Topic
-        mqttClient.subscribe("mtopic", 1)
-        mqttClient.on_message = self.on_message_come  # 消息到来处理函数
+        self.mqttClient.subscribe(self.stopic, 0)
+        self.mqttClient.on_message = self.on_message_come  # 消息到来处理函数
 
     def mainloop(self):
         # 自定义Topic消息上行
-        self.on_publish("mtopic", "Hello msg!", 1)
+        self.on_publish(self.ptopic, "Hello msg!", 1)
         # 系统属性Topic消息上行
-        self.on_publish("mtopic",
+        self.on_publish(self.ptopic,
                    "{\"method\":\"thing.service.property.set\",\"id\":\"1745506903\",\"params\":{\"Status\":1},\"version\":\"1.0.0\"}",
                    1)
-        self.on_subscribe()
 
     def main(self):
         sched = BlockingScheduler()
         self.on_mqtt_connect()
-        sched.add_job(self.mainloop, 'interval', seconds=5)
+        # self.on_subscribe()
+        sched.add_job(self.mainloop, 'interval', seconds=3)
+        self.on_subscribe()
         sched.start()
 
 
-def main():
-    t = MQTTClientWithDB()
-    t.main()
-
-
+# def main():
+#     clientid = 'test_mqtt_python_' + str(uuid.uuid4())
+#
+#     t = MQTTClientWithDB(clientid,"mtopic")
+#     t.main()
 
 if __name__ == "__main__":
     print("Start Test Mqtt client!")
-    main()
+    clientid = 'test_mqtt_python_' + str(uuid.uuid4())
+
+    t = MQTTClientWithDB(clientid, "mtopic","mtopic/#")
+    t.main()
--- mqttClientMsg.py
+++ mqttClientMsg.py
@@ -1,12 +1,5 @@
 import json
-import sys
 import uuid
-import time
-
-import pymysql
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 import paho.mqtt.client as mqtt
 from apscheduler.schedulers.blocking import BlockingScheduler
@@ -14,6 +7,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 # https://github.com/bigdot123456/MACNode/blob/master/MACCheckMySQL.py
 
 # SQLCODE="mysql+pymysql://fastroot:test123456@111.229.168.108/fastroot?charset=UTF8MB4"
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 SQLCODE = "mysql+pymysql://tiger:test123456!@@127.0.0.1/test?charset=utf8"
 # const pool0 = mysql.createPool({
 #   connectionLimit: 50,
@@ -24,27 +20,35 @@ SQLCODE = "mysql+pymysql://tiger:test123456!@@127.0.0.1/test?charset=utf8"
 #   multipleStatements: true //是否允许执行多条sql语句
 # });
 
+MQTTHOST = '111.229.168.108'
+MQTTPORT = 1883
+username = 'userA'
+password = 'userfast'
+
 defaultcfgName = "mqttConfig.json"
 # defaultServer = "104.233.164.173"
 defaultServer = "111.229.168.108"
 defaultPort = 1883
 defaultUsername = 'userA'
 defaultPassword = 'userfast'
-defaultTopic = 'mtopic'
+defaultsTopic = 'mtopic/#'
+defaultpTopic = 'mtopic'
 
-class mqttClientMsg():
-    broker = defaultServer
-    port = defaultPort
-    username = defaultUsername
-    password = defaultPassword
-    topic = defaultTopic
+class MQTTClientWithDB():
+    ptopic=""
+    stopic=""
+    username=""
+    password=""
 
-    clientid = 'test_mqtt_python_' + str(uuid.uuid4())
+    cfgName="mqttConfig.json"
 
-    def __init__(self, cfgName=defaultcfgName):
+    def __init__(self,clientid="MQTTClient", cfgName=defaultcfgName):
         self.cfgName = cfgName
+        self.clientid = clientid
         self.readconfig()
         self.initDB()
+        self.mqttClient = mqtt.Client(f"goog|securemode=3,signmethod=hmacsha1|")
+        self.mqttClient.username_pw_set(self.username, self.password)
 
     def initDB(self):
         self.engine = create_engine(SQLCODE)
@@ -66,24 +70,21 @@ class mqttClientMsg():
                 self.port = temp.get('port', defaultPort)
                 self.username = temp.get('username', defaultUsername)
                 self.password = temp.get('password', defaultPassword)
-                self.topic = temp.get('topic', defaultTopic)
+                self.ptopic = temp.get('mtopic', defaultpTopic)
+                self.stopic = temp.get('mtopic', defaultsTopic)
         except :
             print(f"should check config file {self.cfgName}")
 
-    # 连接MQTT服务器
     def on_mqtt_connect(self):
-        print(f"Start communication with peer: {self.broker}...\n")
-        self.mqttClient = mqtt.Client(f"{self.clientid}|securemode=3,signmethod=hmacsha1|")
-        self.mqttClient.username_pw_set(self.username, self.password)
-        self.mqttClient.connect(self.broker, self.port, 60)
+        self.mqttClient.connect(MQTTHOST, MQTTPORT, 60)
         self.mqttClient.loop_start()
 
     # publish 消息
-    def on_publish(self,topic,payload,qos):
+    def on_publish(self,topic, payload, qos):
         self.mqttClient.publish(topic, payload, qos)
 
     # 消息处理函数
-    def on_message_come(self,client,userdata,msg):
+    def on_message_come(self,client, userdata, msg):
         # print(msg.topic + " " + ":" + str(msg.payload))
         info=json.loads(str(msg.payload))
 
@@ -103,26 +104,35 @@ class mqttClientMsg():
     # subscribe 消息
     def on_subscribe(self):
         # 订阅监听自定义Topic
-        self.mqttClient.subscribe("mtopic", 1)
+        self.mqttClient.subscribe(self.stopic, 0)
         self.mqttClient.on_message = self.on_message_come  # 消息到来处理函数
 
-    # @sched.scheduled_job('cron', id='my_job_id', second=1)
     def mainloop(self):
         # 自定义Topic消息上行
-        self.on_publish("mtopic", "Hello msg!", 1)
+        self.on_publish(self.ptopic, "Hello msg!", 1)
         # 系统属性Topic消息上行
-        self.on_publish("mtopic",
+        self.on_publish(self.ptopic,
                    "{\"method\":\"thing.service.property.set\",\"id\":\"1745506903\",\"params\":{\"Status\":1},\"version\":\"1.0.0\"}",
                    1)
-        self.on_subscribe()
 
     def main(self):
         sched = BlockingScheduler()
         self.on_mqtt_connect()
-        sched.add_job(self.mainloop, 'interval', seconds=5)
+        # self.on_subscribe()
+        sched.add_job(self.mainloop, 'interval', seconds=3)
+        self.on_subscribe()
         sched.start()
+
+
+# def main():
+#     clientid = 'test_mqtt_python_' + str(uuid.uuid4())
+#
+#     t = MQTTClientWithDB(clientid,"mtopic")
+#     t.main()
 
 if __name__ == "__main__":
     print("Start Test Mqtt client!")
-    t=mqttClientMsg()
+    clientid = 'test_mqtt_python_' + str(uuid.uuid4())
+
+    t = MQTTClientWithDB(clientid)
     t.main()
